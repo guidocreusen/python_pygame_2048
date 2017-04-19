a708a293c55b7379b42a2961189ce83c80dabfd2
Guido Creusen <guidocreusen@gmail.com>
2017-04-19 22:03:26 +0200

    added game over fade animation

--- 2048.py
+++ 2048.py
@@ -32,14 +32,35 @@ pygame.display.update()
 #shows the game over message
 def game_over():
 	board.draw()
-	overlay = pygame.Surface((SCR_SIZE[0], SCR_SIZE[1]), pygame.SRCALPHA)
-	overlay.fill((20,20,20,200))
-	scr.blit(overlay, (0,0))
+	pygame.display.update()
 
+	#wait for a certain amount of time (in ms) before trigerring the game over overlay
+	wait_time = 1000
+	animation_time = 2000
+	framerate = 200
+	time_elapsed = 0
+	pygame.time.delay(wait_time)
+	clock = pygame.time.Clock()
+
+	#animation loop
+	while time_elapsed < animation_time:
+		dt = clock.tick(framerate)
+		animation_progress = time_elapsed/animation_time
+
+		#draw the transparent background overlay, fade to white with alpha 200
+		board.draw()
+		overlay = pygame.Surface((SCR_SIZE[0], SCR_SIZE[1]), pygame.SRCALPHA)
+		overlay.fill((255,255,255, 200*animation_progress))
+		scr.blit(overlay, (0,0))
+
+		pygame.display.update()
+		time_elapsed += dt
+	
+	#rounded rect holding game over text
 	game_over_rect = pygame.Rect(0,0, 300, 150)
 	game_over_rect_rounded = AAfilledRoundedRect(game_over_rect, (200,200,200), 0.2)
 	scr.blit(game_over_rect_rounded, (SCR_SIZE[0]/2-150,SCR_SIZE[1]/2-75))
-
+	#game over text message
 	font = pygame.font.SysFont("bold", 60)
 	font_small = pygame.font.SysFont("arial", 15)
 	txt_game_over = font.render("Game Over", True, (50,50,50))
@@ -47,19 +68,15 @@ def game_over():
 	scr.blit(txt_game_over, (SCR_SIZE[0]/2-txt_game_over.get_width()/2,SCR_SIZE[1]/2-txt_game_over.get_height()/2))
 	scr.blit(txt_press_button, (SCR_SIZE[0]/2-txt_press_button.get_width()/2,SCR_SIZE[1]/2-txt_game_over.get_height()/2+50))
 
-
 	pygame.display.update()
 
-	#wait for keypress
+	#wait for keypress or quit, then exit programm (clear event queue first)
+	pygame.event.get()
 	event = pygame.event.wait()
-	while not event.type == KEYDOWN:
+	while not (event.type == KEYDOWN or event.type == QUIT):
 		event = pygame.event.wait()
-		if event.type == QUIT:
-			sys.exit()
 		continue
-
 	sys.exit()
-
 
 #main loop
 while True:
