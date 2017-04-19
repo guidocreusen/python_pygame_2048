78f6a0b6b036482a3df25420e781639a4d393fdb
Guido Creusen <guidocreusen@gmail.com>
2017-04-19 22:25:00 +0200

    moved game over fade animation from 2048.py to board class

--- 2048.py
+++ 2048.py
@@ -26,57 +26,7 @@ key_bindings = {
 }
 
 board.draw()
-
 pygame.display.update()
-
-#shows the game over message
-def game_over():
-	board.draw()
-	pygame.display.update()
-
-	#wait for a certain amount of time (in ms) before trigerring the game over overlay
-	wait_time = 1000
-	animation_time = 2000
-	framerate = 200
-	time_elapsed = 0
-	pygame.time.delay(wait_time)
-	clock = pygame.time.Clock()
-
-	#animation loop
-	while time_elapsed < animation_time:
-		dt = clock.tick(framerate)
-		animation_progress = time_elapsed/animation_time
-
-		#draw the transparent background overlay, fade to white with alpha 200
-		board.draw()
-		overlay = pygame.Surface((SCR_SIZE[0], SCR_SIZE[1]), pygame.SRCALPHA)
-		overlay.fill((255,255,255, 200*animation_progress))
-		scr.blit(overlay, (0,0))
-
-		pygame.display.update()
-		time_elapsed += dt
-	
-	#rounded rect holding game over text
-	game_over_rect = pygame.Rect(0,0, 300, 150)
-	game_over_rect_rounded = AAfilledRoundedRect(game_over_rect, (200,200,200), 0.2)
-	scr.blit(game_over_rect_rounded, (SCR_SIZE[0]/2-150,SCR_SIZE[1]/2-75))
-	#game over text message
-	font = pygame.font.SysFont("bold", 60)
-	font_small = pygame.font.SysFont("arial", 15)
-	txt_game_over = font.render("Game Over", True, (50,50,50))
-	txt_press_button = font_small.render("press any key to exit", True, (50,50,50))
-	scr.blit(txt_game_over, (SCR_SIZE[0]/2-txt_game_over.get_width()/2,SCR_SIZE[1]/2-txt_game_over.get_height()/2))
-	scr.blit(txt_press_button, (SCR_SIZE[0]/2-txt_press_button.get_width()/2,SCR_SIZE[1]/2-txt_game_over.get_height()/2+50))
-
-	pygame.display.update()
-
-	#wait for keypress or quit, then exit programm (clear event queue first)
-	pygame.event.get()
-	event = pygame.event.wait()
-	while not (event.type == KEYDOWN or event.type == QUIT):
-		event = pygame.event.wait()
-		continue
-	sys.exit()
 
 #main loop
 while True:
@@ -97,7 +47,14 @@ while True:
 
 	#game over
 	if board.game_over():
-		game_over()
+		board.game_over_animation()
+		#wait for keypress or quit, then exit programm (clear event queue first)
+		pygame.event.get()
+		event = pygame.event.wait()
+		while not (event.type == KEYDOWN or event.type == QUIT):
+			event = pygame.event.wait()
+			continue
+		sys.exit()
 
 	#draw bg and board
 	scr.fill((240,240,240))
--- board.py
+++ board.py
@@ -205,6 +205,47 @@ class Board(object):
 				return False
 		return True
 
+	#shows the game over message
+	def game_over_animation(self):
+		self.draw()
+		pygame.display.update()
+
+		#wait for a certain amount of time (in ms) before trigerring the game over overlay
+		wait_time = 1000
+		animation_time = 2000
+		framerate = 200
+		time_elapsed = 0
+		pygame.time.delay(wait_time)
+		clock = pygame.time.Clock()
+
+		#animation loop
+		while time_elapsed < animation_time:
+			dt = clock.tick(framerate)
+			animation_progress = time_elapsed/animation_time
+
+			#draw the transparent background overlay, fade to white with alpha 200
+			self.draw()
+			overlay = pygame.Surface((self.scr_size[0], self.scr_size[1]), pygame.SRCALPHA)
+			overlay.fill((255,255,255, 200*animation_progress))
+			self.surface.blit(overlay, (0,0))
+
+			pygame.display.update()
+			time_elapsed += dt
+	
+		#rounded rect holding game over text
+		game_over_rect = pygame.Rect(0,0, 300, 150)
+		game_over_rect_rounded = AAfilledRoundedRect(game_over_rect, (200,200,200), 0.2)
+		self.surface.blit(game_over_rect_rounded, (self.scr_size[0]/2-150,self.scr_size[1]/2-75))
+		#game over text message
+		font = pygame.font.SysFont("bold", 60)
+		font_small = pygame.font.SysFont("arial", 15)
+		txt_game_over = font.render("Game Over", True, (50,50,50))
+		txt_press_button = font_small.render("press any key to exit", True, (50,50,50))
+		self.surface.blit(txt_game_over, (self.scr_size[0]/2-txt_game_over.get_width()/2,self.scr_size[1]/2-txt_game_over.get_height()/2))
+		self.surface.blit(txt_press_button, (self.scr_size[0]/2-txt_press_button.get_width()/2,self.scr_size[1]/2-txt_game_over.get_height()/2+50))
+
+		pygame.display.update()
+
 	#adds a random value of either 2 or 4 on an empty square, returns False if no empty squares otherwise True
 	def add_random_square(self):
 		#return False if no value 0 in flattened squares grid
